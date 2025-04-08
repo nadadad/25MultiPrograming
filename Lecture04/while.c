37f65569c1034ad749db7a2be0318283af3a8c0d
nadadad <dior9795@gmail.com>
2025-04-08 14:52:12 +0900

    LAH

---
+++ Lecture04/while.c
@@ -0,0 +1,12 @@
+﻿int main()
+{
+        int i;
+	
+	i=5;
+	while(i)
+	{
+		puts("사랑해");
+		i=i-1;
+	}
+	return 0;
+}
---
+++ Lecture05/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	if (1)
+	{
+		int i = 1;
+		while (i)
+		{
+			printf("*");
+			i = i - 1;
+		}
+	}
+
+	return 0;
+}
